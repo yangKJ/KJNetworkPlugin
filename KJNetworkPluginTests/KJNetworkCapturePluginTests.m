4c678bb7d8ee6ba07de2820fa4f19e6a05ae2e20
Zz <393103982@qq.com>
2021-08-31 19:23:12 +0800

    test：capture use case

---
+++ KJNetworkPluginTests/KJNetworkCapturePluginTests.m
@@ -0,0 +1,70 @@
+//
+//  KJNetworkCapturePluginTests.m
+//  KJNetworkPluginTests
+//
+//  Created by yangkejun on 2021/8/31.
+//  https://github.com/yangKJ/KJNetworkPlugin
+
+#import <XCTest/XCTest.h>
+#import "KJNetworkManager.h"
+#import "KJNetworkCapturePlugin.h"
+
+@interface KJNetworkCapturePluginTests : XCTestCase
+
+@end
+
+@implementation KJNetworkCapturePluginTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    KJBaseNetworking.baseParameters = @{
+        @"key" : @"value"
+    };
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+// 测试抓包插件
+- (void)testCapturePlugin{
+    
+    XCTestExpectation * expectation = [self expectationWithDescription:@"test capture plugin."];
+    
+    KJNetworkingRequest * request = [[KJNetworkingRequest alloc] init];
+    request.method = KJNetworkRequestMethodGET;
+    request.path = @"/headers";
+    request.responseSerializer = KJSerializerJSON;
+    
+    KJNetworkCapturePlugin * plugin = [KJNetworkCapturePlugin sharedInstance];
+    plugin.openLog = YES;
+    request.plugins = @[plugin];
+    
+    [KJNetworkManager HTTPRequest:request configuration:nil success:^(id  _Nonnull responseObject) {
+        [expectation fulfill];
+    } failure:^(NSError * _Nonnull error) {
+        XCTFail(@"%@", error.localizedDescription);
+    }];
+    
+    [self waitForExpectationsWithTimeout:30 handler:nil];
+    
+}
+
+// 读取抓包数据
+- (void)testReadCapture{
+    XCTestExpectation * expectation = [self expectationWithDescription:@"test read capture data."];
+    
+    [KJNetworkCapturePlugin readAllLogComplete:^(NSArray<KJCaptureResponse *> * _Nonnull logs) {
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:30 handler:nil];
+}
+
+
+@end
